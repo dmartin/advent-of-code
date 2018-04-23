aaa38704b5cd95964794f2404383fbc46a412713
Dustin Martin <martind10@upmc.edu>
2018-04-23 14:43:31 -0400

    Add 2017 day 7 part 1 solution

---
+++ 2017/day7/day7.py
@@ -0,0 +1,30 @@
+import sys
+import re
+import collections
+
+infile = sys.stdin.read().strip().split('\n')
+
+# For each line, extract the node and its children
+Entry = collections.namedtuple('Entry', ['node', 'weight', 'children'])
+entries = []
+p = re.compile(r'^(\w*) \((\d*)\)( -> )?(.*)?$')
+for line in infile:
+    m = p.search(line).groups()
+    e = Entry(m[0], m[1], m[3])
+    entries.append(e)
+
+# Build a parent -> child lookup table
+parent = {}
+for entry in entries:
+    children = entry.children.split(', ')
+    for child in children:
+        parent[child] = entry.node
+
+# Traverse the table to the root
+curr = list(parent.keys())[0]
+while(True):
+    try:
+        curr = parent[curr]
+    except KeyError:
+        break
+print(curr)
